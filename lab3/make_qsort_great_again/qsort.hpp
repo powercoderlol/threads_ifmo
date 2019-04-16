355e4b40be5056b02ecd7f92dd45d0a2c37cc0b3
Ivan.Poliakov <ipolyiakov@gmail.com>
2019-04-16 22:27:11 +0300

    MAKE QSORT GREAT AGAIN

---
+++ lab3/make_qsort_great_again/qsort.hpp
@@ -0,0 +1,32 @@
+#pragma once
+// let's make qsort great again
+// there is simple fp-like implementation of qsort algorithm:
+// get list by value and return by value not like std::sort()
+#include <algorithm>
+#include <list>
+
+namespace algo {
+
+template<class T>
+std::list<T> qsort(std::list<T> input) {
+    if(input.empty())
+        return input;
+    std::list<T> result;
+    result.splice(result.begin(), input, input.begin());
+    T const& mediana = *result.begin();
+    auto devider = std::partition(
+        input.begin(), input.end(),
+        [&](T const& val) { return val < mediana; });
+
+    std::list<T> less;
+    less.splice(less.end(), input, input.begin(), devider);
+
+    auto new_less(qsort(std::move(less)));
+    auto more(qsort(std::move(input)));
+
+    result.splice(result.end(), more);
+    result.splice(result.begin(), new_less);
+    return result;
+}
+
+} // namespace algo
